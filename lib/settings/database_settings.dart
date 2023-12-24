9b1804d8b65c7b5cd6c62c2efaa9d5d1d2648968
naufal-alwi-p <naufalpratama992@gmail.com>
2023-12-24 19:17:21 +0700

    Membuat file yang berisi koneksi ke database tidak di-upload ke repository

--- lib/settings/database_settings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mysql1/mysql1.dart';
-
-ConnectionSettings settingsDB = ConnectionSettings(
-  host: '52.77.239.216',
-  port: 3306,
-  user: 'user1',
-  password: 'lampp',
-  db: 'aplikasi_daftar_belanja'
-);
